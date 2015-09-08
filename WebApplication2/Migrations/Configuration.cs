0e2536505d6bf7375aba549caf33c62541e50512
lejtmany <lejtmany@gmail.com>
2015-09-08 14:40:17 -0400

    added rating

--- WebApplication2/Migrations/Configuration.cs
+++ WebApplication2/Migrations/Configuration.cs
@@ -22,7 +22,8 @@ namespace WebApplication2.Migrations
             Title = "When Harry Met Sally",
             ReleaseDate = DateTime.Parse("1989-1-11"),
             Genre = "Romantic Comedy",
-            Price = 7.99M
+            Price = 7.99M,
+            Rating = "PG"
         },
 
          new Movie
@@ -30,7 +31,8 @@ namespace WebApplication2.Migrations
              Title = "Ghostbusters ",
              ReleaseDate = DateTime.Parse("1984-3-13"),
              Genre = "Comedy",
-             Price = 8.99M
+             Price = 8.99M,
+             Rating = "PG"
          },
 
          new Movie
@@ -38,7 +40,8 @@ namespace WebApplication2.Migrations
              Title = "Ghostbusters 2",
              ReleaseDate = DateTime.Parse("1986-2-23"),
              Genre = "Comedy",
-             Price = 9.99M
+             Price = 9.99M,
+             Rating = "PG"
          },
 
        new Movie
@@ -46,7 +49,8 @@ namespace WebApplication2.Migrations
            Title = "Rio Bravo",
            ReleaseDate = DateTime.Parse("1959-4-15"),
            Genre = "Western",
-           Price = 3.99M
+           Price = 3.99M,
+           Rating = "PG"
        }
    );
         }
